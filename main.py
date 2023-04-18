294eb4aad66ae899ec9ea58516cd4212d273c87e
Mustajab Azam <33552037+mustajabazam@users.noreply.github.com>
2023-04-17 21:05:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ parser.add_argument('--env-name', default="Walker2d-v2",
                     help='Mujoco Gym environment (default: HalfCheetah-v2)')
 parser.add_argument('--policy', default="Gaussian",
                     help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
+parser.add_argument('--optimizer_class', default="SGDHess",
+                    help='Optimizer class to use: Adam | AdamW | SGD | SGDHess (default: Gaussian)')
 parser.add_argument('--eval', type=bool, default=True,
                     help='Evaluates a policy a policy every 10 episode (default: True)')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
